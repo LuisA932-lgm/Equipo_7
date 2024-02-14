494d7671438bc69cd54a86caea6ca5ff32520623
Luis01-AE <157388343+LuisA932-lgm@users.noreply.github.com>
2024-02-13 19:29:32 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Inicio} from './Components/Inicio'
 import { useEffect, useState } from 'react'
 import { Registtro } from './Components/Registtro';
 import './App.css'
-
+import {BrowseRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
 
@@ -17,18 +17,25 @@ function App() {
   const [user, setUser] = useState([])
   
   return (
-   
+  <Router> 
       
   <div className ="App"> 
+
+    <Switch>
+      <Route path="/">
+        <Registtro />
+      </Route>
+        </Switch>
    
      { 
       !user.length > 0 ? <Formulario  setUser = {setUser}/> 
       : <Inicio user={user} setUser={setUser}/>    
     }  
-    
+   
       
  
     </div>
+    </Router>
 )
 
   }
